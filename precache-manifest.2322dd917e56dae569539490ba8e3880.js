5f956c092161968faccb46019d00998e4025e43b
Code Sandbox <hello@codesandbox.io>
2021-06-20 08:40:35 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2322dd917e56dae569539490ba8e3880.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f67caf6d038fff56b541180342e4e4",
+    "url": "https://eelyneee.github.io/csb-w82qw/index.html"
+  },
+  {
+    "revision": "c0e544843cfa412027e8",
+    "url": "https://eelyneee.github.io/csb-w82qw/static/js/2.97bd378f.chunk.js"
+  },
+  {
+    "revision": "8cbf5eb6725b02c4a4f8",
+    "url": "https://eelyneee.github.io/csb-w82qw/static/js/main.e5d598d4.chunk.js"
+  },
+  {
+    "revision": "fc15d2e5e1116b5c505c",
+    "url": "https://eelyneee.github.io/csb-w82qw/static/js/runtime-main.a8a902b6.js"
+  }
+]);
